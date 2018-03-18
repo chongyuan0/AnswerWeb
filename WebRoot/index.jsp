72eba6c72f4bc7e9cf2049f1ed8c2e353d70abc0
yuan <chongyuan1997@qq.com>
2018-03-18 12:57:51 +0800

    新工程和添加数据库文件

---
+++ WebRoot/index.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <base href="<%=basePath%>">
+    
+    <title>My JSP 'index.jsp' starting page</title>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+	<meta http-equiv="description" content="This is my page">
+	<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+  </head>
+  
+  <body>
+    This is my JSP page. <br>
+    <a href="test">用于测试是否搭建成功：点击链接查询answerWeb数据库中的test表</a>
+  </body>
+</html>
---
+++ database.sql
@@ -0,0 +1,70 @@
+-- 创建数据库
+drop database if exists answerWeb;
+create database answerWeb;
+use answerWeb;
+
+-- 创建用户表
+create table user(
+	userno int auto_increment primary key, --自增主键id
+	username varchar(20) not null,	--用户名
+	password varchar(40) not null,	--用户密码
+	email varchar(50),	--用户邮箱
+	sex varchar(2),		--用户性别
+	status int 			--用户是否通过邮箱验证 , 0未通过, 1通过
+);
+
+-- 创建题目类型表
+create table questiontype(
+	typeno int auto_increment primary key,	-- 自增主键id
+	typename varchar(30) not null,		-- 题目类型Id
+	belongtypeno int,		-- 属于哪个题目类型id
+	constraint fk_questiontype_belongtypeno foreign key(belongtypeno) 
+		references questiontype(typeno)
+);
+
+-- 创建题目表
+create table question(
+	questionno int auto_increment primary key,	-- 自增主键id
+	content varchar(200) not null,	-- 题目内容,可能是文本或链接
+	description varchar(200),		-- 题目涉及知识描述,用于答题后, 文本或链接
+	constatus int not null,	-- 内容类型, 1文本, 2图片, 3视频, 4音乐
+	desstatus int,	-- 描述类型, 1文本, 2图片
+	typeno int not null,		-- 题目类型id
+	constraint fk_question_typeno foreign key(typeno)
+		references questiontype(typeno)
+);
+
+-- 创建题目选项表
+create table options(
+	optionsno int auto_increment primary key, -- 自增主键id
+	questionno int not null, -- 题目id
+	content varchar(200) not null, -- 选项内容
+	status int,	-- 选项对错, 1对 0错
+	constraint fk_options_questionno foreign key(questionno)
+		references question(questionno)
+);
+
+-- 创建记录表
+create table records(
+	recordsno int auto_increment primary key, -- 自增主键id
+	userno int not null, -- 用户id
+	typeno int not null, -- 类型id
+	acnumber int default 0,	-- 答对题数
+	erunmber int default 0, -- 答错题数
+	constraint fk_records_userno foreign key(userno)
+		references user(userno),
+	constraint fk_records_typeno foreign key(typeno)
+		references questiontype(typeno)
+);
+
+-- 创建管理员表
+create table admins(
+	adminid int auto_increment primary key, -- 自增主键id
+	adminname varchar(30) not null,	-- 管理员名称
+	password varchar(40) not null,	-- 管理员密码
+	adminflag int	-- 管理员权限, 1普通 2超级
+);
+
+-- 插入数据
+insert into user(username,password,email,sex,status) values('yuan','yuan','123','男',1);
+
